700fbc0b4245975f2ea5f287a561e82b9caf4c2a
Thanh Tin <thanhtin96@gmail.com>
2016-12-20 02:59:51 +0700

    thêm utf8 cho connect

--- WebNCKH_LTW/src/Packages/DBConnect.java
+++ WebNCKH_LTW/src/Packages/DBConnect.java
@@ -7,9 +7,12 @@ public class DBConnect {
 	public static Connection getConnecttion() {
         Connection cons = null;
         try {
+        	String dbname="nhom15";
+        	String dbuser="nhom15";
+        	String dbpass="123456";
             Class.forName("com.mysql.jdbc.Driver");
             cons = DriverManager.getConnection(
-                    "jdbc:mysql://72.13.93.206:3307/nhom15", "nhom15", "123456");
+            "jdbc:mysql://72.13.93.206:3307/"+dbname+"?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8", dbuser, dbpass);
         } catch (Exception e) {
             e.printStackTrace();
         }
